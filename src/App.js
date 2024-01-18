e0071d0c3b111856bf9cfb5c6f6a8bc5e5dad319
Bumbobin <ndthung281@gmail.com>
2024-01-18 20:25:13 +0700

    Structure create

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Router from './components/routes/Router';
+
+function App() {
+  return (
+    <div className="App">
+      <Router/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/routes/AppRoutes.js
@@ -0,0 +1,11 @@
+import Home from "../home/Home";
+
+
+const AppRoutes = [
+    {
+        path: "/",
+        element: <Home/>
+    }
+]
+
+export default AppRoutes;
