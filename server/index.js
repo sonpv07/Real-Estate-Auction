331b8de71516c7b5eda9b8ba275938ec559e8f52
Vinh Son <sonpv07@gmail.com>
2024-01-18 22:09:32 +0700

    File Architecture

--- server/index.js
+++ server/index.js
@@ -1,18 +1,29 @@
+// DEFINE LIBRARY
 const express = require("express");
-const mongoose = require("mongoose");
 const cors = require("cors");
+require("dotenv").config();
 
+// DEFINE DATABASE
+const db = require("./config/database");
+
+// DEFINE ROUTER
+const realEstateRouter = require("./router/realEstateRouter");
+
+// DEFINE EXPRESS
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
-require("dotenv").config();
-
+// DEFINE PORT
 const port = parseInt(process.env.PORT) || 5000;
 
-const router = express.Router();
+// DEFINE ROUTER LINK
+app.use("/", realEstateRouter);
 
+// CONNECT TO PORT
 app.listen(port, (req, res) => {
   console.log(`Listen port: ${port}`);
 });
+
+// CONNECT DATABASE
+db.connect();
