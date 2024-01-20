c25422b05d1e4f3a7fad695270acfea6e0a1b0fe
Bumbobin <ndthung281@gmail.com>
2024-01-20 11:07:16 +0700

    navbar

---
+++ src/components/auth/signIn/SignIn.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+const SignIn = () => {
+    return (
+        <div className="return-container">
+            SignIn
+        </div>
+    )
+}
+
+export default SignIn
---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,12 @@
+import ResponsiveAppBar from "./Navbar";
+
+
+const Layout = () => {
+    return (
+        <div className="layout-container">
+            <ResponsiveAppBar/>
+        </div>
+    )
+}
+
+export default Layout;
